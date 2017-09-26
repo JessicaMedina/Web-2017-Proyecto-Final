e8b4158eac20421b869ffba7a42d58c7a4ef42a6
Jenny <Jenny@Jenny-PC>
2017-09-26 12:00:14 -0500

    Inicio

--- index.php
+++ index.php
@@ -4,13 +4,30 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Página de inicio de sesión</title>
+        <link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/login.css" rel="stylesheet">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php      
-        ?>
-    </body>
-</html>
+        <div class="container">
+            <?php require_once 'templates/ads.php'; ?>
+            <div class="ligin-form">
+                <?php require_once 'templates/message.php'; ?>
+                <div class="form-header">
+                    <i class="fa fa-user"></i>
+                </div>
+                <?php
+                ?>
+                </body>
+                </html>
